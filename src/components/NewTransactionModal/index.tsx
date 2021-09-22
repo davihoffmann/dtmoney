593f4c008c3c8f15c43015df447ac8a0314f6de9
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-09-21 21:25:28 -0300

    create route post for transactions on mirage

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
+import { api } from '../../services/api';
+
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -21,9 +23,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
-      title, value, category, type
-    })
+    const data = {
+      title, 
+      value, 
+      category, 
+      type
+    };
+
+    api.post('/transactions', data)
   }
 
   return(
