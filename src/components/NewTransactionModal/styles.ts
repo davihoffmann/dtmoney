9d24d005b06e05aa3a4c44d1bc014492d07c80ba
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-09-21 21:01:59 -0300

    change style when type of transaction is selected

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -62,6 +62,12 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' |'red' ;
+}
+
+const color = {
+  green: '#33cc95',
+  red: '#e52e4d',
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -69,7 +75,9 @@ export const RadioBox = styled.button<RadioBoxProps>`
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive ? '#eee3' : 'transparent'};
+  background: ${(props) => props.isActive 
+    ? transparentize(0.9, color[props.activeColor])
+    : 'transparent'};
 
   display: flex;
   align-items: center;
