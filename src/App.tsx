4f7cd01d0d69ed8b27defcf9070abd0525a1deca
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-09-22 14:40:34 -0300

    create context for share transactions list

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
 
+import { TransactionsProvider } from './TransactionsContext';
+
 import { GlobalStyle } from './styles/global';
 
-Modal.setAppElement('#root');
+Modal.setAppElement('#root'); 
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -21,11 +23,11 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
